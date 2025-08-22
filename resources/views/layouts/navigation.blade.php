b72e6dba9979bfb1c21118776dca21bf2ebff8c3
softpromani <softpromani@gmail.com>
2025-08-22 19:17:06 +0530

    initial commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="{{ route('admin.dashboard') }}">
+                        <img src="{{ asset('logo/logo.jpeg') }}" alt="Logo" class="w-20 h-20">
                     </a>
                 </div>
 
